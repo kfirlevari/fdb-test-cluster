877be911120864eb7f1df9c9b5b5232e1b88de6a
Kfir Lev-Ari <klevari@apple.com>
2018-11-26 19:33:51 +0200

    Fix run ycsb.sh

--- packer/upload/run_ycsb.sh
+++ packer/upload/run_ycsb.sh
@@ -8,8 +8,8 @@ mkdir ./"$logsfoldername"
 for workload in a b c d e f
 do
     echo "Testing fdbrecordlayer workload $workload"
-    ./bin/ycsb load fdbrecordlayer -threads 8 -P "workloads/workload$workload" -p foundationdb.clusterfile=/etc/foundationdb/fdb.cluster > ./"$logsfoldername"/recdb."w$workload.load".csv
-    ./bin/ycsb run fdbrecordlayer -threads 8 -P "workloads/workload$workload" -p foundationdb.clusterfile=/etc/foundationdb/fdb.cluster  > ./"$logsfoldername"/recdb."w$workload.run".csv
+    ./bin/ycsb load fdbrecordlayer -threads 8 -P "workloads/workload$workload" -p fdbrecordlayer.clusterfile=/etc/foundationdb/fdb.cluster > ./"$logsfoldername"/recdb."w$workload.load".csv
+    ./bin/ycsb run fdbrecordlayer -threads 8 -P "workloads/workload$workload" -p fdbrecordlayer.clusterfile=/etc/foundationdb/fdb.cluster  > ./"$logsfoldername"/recdb."w$workload.run".csv
     
     echo "Testing foundationdb workload $workload"
     ./bin/ycsb load foundationdb -threads 8 -P "workloads/workload$workload" -p foundationdb.clusterfile=/etc/foundationdb/fdb.cluster -p foundationdb.saveserialized=false > ./"$logsfoldername"/fdb."w$workload.load".csv
